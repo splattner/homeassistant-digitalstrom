c1a91be18f4178909da9fe40c5a0615994d08ad8
Sebastian Plattner <plattner@puzzle.ch>
2021-08-08 16:26:01 +0200

    add separate energy meter

--- custom_components/digitalstrom/sensor.py
+++ custom_components/digitalstrom/sensor.py
@@ -4,7 +4,7 @@ from typing import Callable, Union
 
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import POWER_WATT, CONF_HOST, CONF_PORT
+from homeassistant.const import POWER_WATT, ENERGY_WATT_HOUR, CONF_HOST, CONF_PORT
 from homeassistant.helpers.typing import ConfigType, HomeAssistantType
 from .pydigitalstrom.client import DSClient
 from .pydigitalstrom.devices.meter import DSMeter
@@ -50,8 +50,13 @@ async def async_setup_entry(
                 hass=hass, dsmeter=meter
             )
         )
+        devices.append(
+            DigitalstromEnergyMeter(
+                hass=hass, dsmeter=meter
+            )
+        )
 
-    device: DigitalstromMeter
+    device: Union[DigitalstromMeter,DigitalstromEnergyMeter]
     async_add_entities(device for device in devices)
 
 
@@ -106,3 +111,55 @@ class DigitalstromMeter(SensorEntity):
 
     async def async_update(self, **kwargs) -> None:
         self._state = await self._dsmeter.get_latest()
+
+class DigitalstromEnergyMeter(SensorEntity):
+    def __init__(
+        self,
+        hass: HomeAssistantType,
+        dsmeter: DSMeter,
+        *args,
+        **kwargs,
+    ):
+        self._hass: HomeAssistantType = hass
+        self._dsmeter: DSMeter = dsmeter
+        self._state: int = None
+        super().__init__(*args, **kwargs)
+
+
+    @property
+    def name(self) -> str:
+        return self._dsmeter.name
+
+    @property
+    def unique_id(self) -> str:
+        return f"dsenergymeter_{self._dsmeter.unique_id}"
+
+    @property
+    def available(self) -> bool:
+        return True
+
+    @property
+    def state(self) -> int:
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit the value is expressed in."""
+        return ENERGY_WATT_HOUR
+
+    @property
+    def device_class(self):
+        return "energy"
+
+    @property
+    def device_info(self) -> dict:
+        """Return information about the device."""
+        return {
+            "identifiers": {(DOMAIN, self._dsmeter.unique_id)},
+            "name": self._dsmeter.name,
+            "model": "DSMeter",
+            "manufacturer": "digitalSTROM AG",
+        }
+
+    async def async_update(self, **kwargs) -> None:
+        self._state = await self._dsmeter.get_latest_energy()
