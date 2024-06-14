254f10a5f4c55931594efb282795bfa342e1902a
Sebastian Plattner <sebastian.plattner@gmail.com>
2024-06-14 10:23:20 +0200

    fix deprecated calls

--- custom_components/digitalstrom/__init__.py
+++ custom_components/digitalstrom/__init__.py
@@ -106,16 +106,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # start websocket listener and action delayer loops on hass startup
     async def digitalstrom_start_loops(event):
         _LOGGER.info(f"loops started for digitalSTROM server at {client.host}")
-        hass.async_add_job(listener.start)
-        hass.async_add_job(client.stack.start)
+        hass.async_create_background_task(listener.start)
+        hass.async_create_background_task(client.stack.start)
 
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_START, digitalstrom_start_loops)
 
     # start websocket listener and action delayer loops on hass shutdown
     async def digitalstrom_stop_loops(event):
         _LOGGER.info(f"loops stopped for digitalSTROM server at {client.host}")
-        hass.async_add_job(client.stack.stop)
-        hass.async_add_job(listener.stop)
+        hass.async_create_background_task(client.stack.stop)
+        hass.async_create_background_task(listener.stop)
 
     hass.bus.async_listen_once(EVENT_HOMEASSISTANT_STOP, digitalstrom_stop_loops)
 
